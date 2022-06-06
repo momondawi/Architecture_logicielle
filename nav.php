c210fc0ed2659605f1b9f6f5a8dff6e00123f097
momo <macbookpro@192.168.1.106>
2022-06-06 13:10:04 +0000

    v1

---
+++ nav.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Actualites</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+
+    <nav>
+        <h2>ACTUALITÉS POLYTECHNICIENNES</h2>
+        <div class="trait"></div>
+        <div id="types">
+            <div class="t1"></div>
+            <a href="./index.php">Accueil</a>
+            <div class="t2"></div>
+            <a href="./sport.php">Sport</a>
+            <div class="t3"></div>
+            <a href="./sante.php">Santé</a>
+            <div class="t4"></div>
+            <a href="./education.php">Education</a>
+            <div class="t5"></div>
+            <a href="./politique.php">Politique</a>
+            <div class="t6"></div>
+        </div>
+    </nav>
+</body>
+</html>
---
+++ sport.php
@@ -0,0 +1,25 @@
+<?php
+
+    include('nav.php');
+    $dsn='mysql:dbname=mglsi_news;host=127.0.0.1;port=7979';
+    $username='root';
+    $pwd='root';
+    
+
+    try{
+
+        $bd=new PDO($dsn,$username,$pwd);
+        $req=$bd->query('SELECT titre, contenu from Article where categorie =1');
+
+        if($req==false){
+            die("Erreur");
+        }
+    }catch (PDOException $e){
+        echo $e->getMessage();
+    }
+
+    while($row=$req->fetch(PDO::FETCH_ASSOC)){
+        ?><h1 style="text-align: center;"><?php echo htmlspecialchars($row['titre']); ?> </h1>
+        <h2><?php echo htmlspecialchars($row['contenu']); ?></h2> <?php
+    }
+?>
